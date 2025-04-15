0d8be836c9d3d05b06530d9c4346e54103eb371f
LuSchumacher <lu.schumacher@bluewin.ch>
2025-04-15 11:18:50 +0200

    update repo

---
+++ data/data_prep.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(magrittr)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+df <- read.csv2('../data/raw_data.csv')
+
+df %<>%
+  select(
+    id, trial, session, statement_type,
+    factual_truth, responses, dur_trial
+  ) %>% 
+  rename(
+    stim_type = statement_type,
+    correct_resp = factual_truth,
+    resp = responses,
+    rt = dur_trial
+  ) %>% 
+  mutate(
+    stim_type = as.factor(stim_type),
+    correct_resp = ifelse(correct_resp == 'true', 1, 0)
+  ) %>% 
+  mutate(correct = ifelse(correct_resp == resp, 1, 0)) %>% 
+  filter(rt > 0) %>% 
+  group_by(id) %>% 
+  filter(
+    log(rt) > boxplot(log(rt))$stats[1],
+    log(rt) < boxplot(log(rt))$stats[5]
+  ) %>% 
+  ungroup() %>% 
+  mutate(id = dense_rank(id))
+
+summary <- df %>% 
+  group_by(id, session) %>% 
+  summarise(
+    N = n(),
+    min = min(rt),
+    max = max(rt),
+    mean = mean(rt),
+  )
+
+# save session data
+df_session_1 <- df %>% 
+  filter(session == 1) %>% 
+  write_csv(., '../data/data_session_1.csv')
+
+df_session_2 <- df %>% 
+  filter(session == 2) %>% 
+  write_csv(., '../data/data_session_2.csv')
+
+
+################################################################################
+# EXPERIMENT 2
+################################################################################
+df <- read_csv2('../data/exp2_raw_data.csv')
+
+df %<>% 
+  rename(
+    id = VP_Code,
+    factual_truth = FactualTruth,
+    stim_type = repetition,
+    resp = responses,
+    rt = responses_rt,
+    rt_question = key_resp_question_rt
+  ) %>% 
+  mutate(
+    id = dense_rank(id),
+    factual_truth = as.numeric(factual_truth),
+    correct = ifelse(resp == factual_truth, 1, 0),
+    rt = as.numeric(rt),
+    rt_question = as.numeric(rt_question)
+  ) %>% 
+  select(
+    id, stim_type, factual_truth,
+    resp, correct, rt, rt_question
+  ) %>% 
+  arrange(id) %>% 
+  group_by(id) %>% 
+  mutate(
+    rt = ifelse(log(rt) > boxplot(log(rt))$stats[1], rt, NA),
+    rt = ifelse(log(rt) < boxplot(log(rt))$stats[5], rt, NA),
+    rt_question = ifelse(log(rt_question) > boxplot(log(rt_question))$stats[1], rt_question, NA),
+    rt_question = ifelse(log(rt_question) < boxplot(log(rt_question))$stats[5], rt_question, NA),
+    rt_question = ifelse(rt_question < 1, NA, rt_question)
+  ) %>% 
+  ungroup() %>% 
+  drop_na(rt, rt_question)
+
+write_csv(df, 'data_exp_2.csv')
--- src/model_fitting.R
+++ src/model_fitting.R
@@ -1,40 +1,8 @@
 library(tidyverse)
 library(magrittr)
-library(rstan)
-library(bayesplot)
 library(cmdstanr)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
-PARAM_NAMES <- c("v", "a", "bias", "ndt", "ndt_s")
-
-df_session_1 <- read.csv('../data/data_session_1.csv')
-df_session_2 <- read.csv('../data/data_session_2.csv')
-
-df_session_1 %<>%
-  # filter(id <= 30) %>%
-  mutate(
-    resp = resp + 1,
-    correct_resp = correct_resp + 1,
-    stim_type = stim_type + 1,
-    condition = case_when(
-      correct_resp == 2 & stim_type == 1 ~ 1,
-      correct_resp == 2 & stim_type == 2 ~ 2,
-      correct_resp == 1 & stim_type == 1 ~ 3,
-      correct_resp == 1 & stim_type == 2 ~ 4
-    )
-  )
-
-N <- length(unique(df_session_1$id))
-
-stan_data = list(
-  `T`        = nrow(df_session_1),
-  N          = N,
-  subject_id = df_session_1$id,
-  resp       = df_session_1$resp,
-  condition  = df_session_1$condition,
-  rt         = df_session_1$rt
-)
 
 init_fun = function(chains=4){
   L = list()
@@ -46,37 +14,157 @@ init_fun = function(chains=4){
       sigma_a    = 0.4 + runif(4, -0.05, 0.05),
       mu_bias    = 0.0 + runif(4, -0.05, 0.05),
       sigma_bias = 0.1 + runif(4, -0.05, 0.05),
-      mu_ndt     = -1 + runif(4, -0.05, 0.05),
+      mu_ndt     = 1 + runif(4, -0.05, 0.05),
       sigma_ndt  = 0.1 + runif(4, -0.01, 0.01),
-      v    = matrix(1.0 + runif(4 * N, -0.5, 0.5), 4, N),
-      a    = matrix(2.0 + runif(4 * N, -0.1, 0.1), 4, N),
-      bias = 0.5 + runif(4 * N, -0.05, 0.05),
-      ndt  = matrix(0.3 + runif(4 * N, -0.01, 0.01), 4, N),
-      ndt_s = 0.05 + runif(1, -0.01, 0.01)
+      ndt_s      = 0.05 + runif(1, -0.01, 0.01),
+      z_v        = matrix(runif(4*N, -1, 1), nrow=4),
+      z_a        = matrix(runif(4*N, -1, 1), nrow=4),
+      z_ndt      = matrix(runif(4*N, -1, 1), nrow=4),
+      z_bias     = matrix(runif(4*N, -1, 1), nrow=4),
+      trel       = matrix(runif(4*N, 0.01, 0.99), nrow=4)
     )
   }
   return(L)
 }
 
+PARAM_NAMES <- c(
+  "transf_mu_v[1]", "transf_mu_v[2]", "transf_mu_v[3]", "transf_mu_v[4]",
+  "transf_mu_a[1]", "transf_mu_a[2]", "transf_mu_a[3]", "transf_mu_a[4]",
+  "transf_mu_ndt[1]", "transf_mu_ndt[2]", "transf_mu_ndt[3]", "transf_mu_ndt[4]",
+  "transf_mu_bias[1]", "transf_mu_bias[2]", "transf_mu_bias[3]", "transf_mu_bias[4]"
+)
+
 m_full <- cmdstan_model(
-  '../model/model_full_2.stan',
+  '../model/model_new.stan',
   cpp_options = list(stan_threads = T)
 )
 
-m_full_fit <- m_full$sample(
+################################################################################
+# SESSION 1
+################################################################################
+df_session_1 <- read.csv('../data/data_session_1.csv')
+df_session_1 %<>%
+  mutate(
+    condition = case_when(
+      factual_truth == 1 & stim_type == 0 ~ 1, # true statement, new
+      factual_truth == 1 & stim_type == 1 ~ 2, # true statement, repeated
+      factual_truth == 0 & stim_type == 0 ~ 3, # false statement, new
+      factual_truth == 0 & stim_type == 1 ~ 4  # false statement, repeated
+    )
+  )
+
+N <- length(unique(df_session_1$id))
+stan_data = list(
+  `T`           = nrow(df_session_1),
+  N             = N,
+  subject_id    = df_session_1$id,
+  resp          = df_session_1$resp,
+  factual_truth = df_session_1$factual_truth,
+  condition     = df_session_1$condition,
+  rt            = df_session_1$rt,
+  minRT         = tapply(df_session_1$rt, list(df_session_1$condition, df_session_1$id), min)
+)
+
+fit_session_1 <- m_full$sample(
   data = stan_data,
   init = init_fun(),
   max_treedepth = 10,
-  adapt_delta = 0.95,
+  adapt_delta = 0.9,
   refresh = 10,
   iter_sampling = 2000,
   iter_warmup = 2000,
   chains = 4,
   parallel_chains = 4,
   threads_per_chain = 2,
-  save_warmup = TRUE,
-  save_cmdstan_config = TRUE
+  save_warmup = TRUE
 )
 
-m_full_fit$save_object("../fits/session_1_full_model")
+fit_session_1$summary(variables = PARAM_NAMES)
+fit_session_1$save_object("../fits/fit_session_1")
+
+################################################################################
+# SESSION 2
+################################################################################
+df_session_2 <- read.csv('../data/data_session_2.csv')
+df_session_2 %<>%
+  mutate(
+    condition = case_when(
+      factual_truth == 1 & stim_type == 0 ~ 1, # true statement, new
+      factual_truth == 1 & stim_type == 1 ~ 2, # true statement, repeated
+      factual_truth == 0 & stim_type == 0 ~ 3, # false statement, new
+      factual_truth == 0 & stim_type == 1 ~ 4  # false statement, repeated
+    )
+  )
+
+N <- length(unique(df_session_2$id))
+stan_data = list(
+  `T`           = nrow(df_session_2),
+  N             = N,
+  subject_id    = df_session_2$id,
+  resp          = df_session_2$resp,
+  factual_truth = df_session_2$factual_truth,
+  condition     = df_session_2$condition,
+  rt            = df_session_2$rt,
+  minRT         = tapply(df_session_2$rt, list(df_session_2$condition, df_session_2$id), min)
+)
+
+fit_session_2 <- m_full$sample(
+  data = stan_data,
+  init = init_fun(),
+  max_treedepth = 10,
+  adapt_delta = 0.9,
+  refresh = 10,
+  iter_sampling = 2000,
+  iter_warmup = 2000,
+  chains = 4,
+  parallel_chains = 4,
+  threads_per_chain = 2,
+  save_warmup = TRUE
+)
+
+fit_session_2$summary(variables = PARAM_NAMES)
+fit_session_2$save_object("../fits/fit_session_2")
+
+################################################################################
+# EXPERIMENT 2
+################################################################################
+df_exp_2 <- read.csv('../data/data_exp_2.csv')
+df_exp_2 %<>%
+  mutate(
+    condition = case_when(
+      factual_truth == 1 & stim_type == 0 ~ 1, # true statement, new
+      factual_truth == 1 & stim_type == 1 ~ 2, # true statement, repeated
+      factual_truth == 0 & stim_type == 0 ~ 3, # false statement, new
+      factual_truth == 0 & stim_type == 1 ~ 4  # false statement, repeated
+    )
+  )
+
+N <- length(unique(df_exp_2$id))
+stan_data = list(
+  `T`           = nrow(df_exp_2),
+  N             = N,
+  subject_id    = df_exp_2$id,
+  resp          = df_exp_2$resp,
+  factual_truth = df_exp_2$factual_truth,
+  condition     = df_exp_2$condition,
+  rt            = df_exp_2$rt,
+  minRT         = tapply(df_exp_2$rt, list(df_exp_2$condition, df_exp_2$id), min)
+)
+
+fit_exp_2 <- m_full$sample(
+  data = stan_data,
+  init = init_fun(),
+  max_treedepth = 10,
+  adapt_delta = 0.9,
+  refresh = 10,
+  iter_sampling = 2000,
+  iter_warmup = 2000,
+  chains = 4,
+  parallel_chains = 4,
+  threads_per_chain = 2,
+  save_warmup = TRUE
+)
+
+fit_exp_2$summary(variables = PARAM_NAMES)
+fit_exp_2$save_object("../fits/fit_exp_2")
 
